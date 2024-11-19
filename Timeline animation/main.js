0a7574c1b0c70801cc0b1336d868609d9008fc77
daddyjanno <jn.drugmand@gmail.com>
2024-11-19 16:41:59 +0100

    main.js

--- Timeline animation/main.js
+++ Timeline animation/main.js
@@ -1,1 +1,51 @@
-console.log('Timeline animation')
+console.log('timeline animation')
+
+const animation = anime.timeline({
+    autoplay: false,
+})
+
+animation
+    .add({
+        targets: '#btn',
+        top: '1500px',
+        duration: 500,
+        easing: 'easeInOutSine',
+    })
+    .add({
+        targets: '#stars',
+        top: '0px',
+        duration: 500,
+        easing: 'easeInOutSine',
+    })
+    .add({
+        targets: '#mountains_behind',
+        bottom: '0px',
+        duration: 500,
+        easing: 'easeInOutSine',
+    })
+    .add({
+        targets: '#moon',
+        top: '0px',
+        duration: 500,
+        easing: 'easeInOutBack',
+    })
+    .add({
+        targets: '#mountains_front',
+        bottom: '0px',
+        duration: 500,
+        easing: 'easeInOutSine',
+    })
+    .add({
+        targets: '#text',
+        marginRight: '0px',
+        duration: 1000,
+        easing: 'easeInOutSine',
+    })
+    .add({
+        targets: '#btn',
+        top: '50%',
+        duration: 1000,
+        easing: 'easeInOutBack',
+    })
+
+document.querySelector('#btn').onclick = animation.play
