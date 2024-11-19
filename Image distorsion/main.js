1bd8f94d2dad1940b59336e24864f5f5d2b16bf9
daddyjanno <jn.drugmand@gmail.com>
2024-11-19 16:50:12 +0100

    main

--- Image distorsion/main.js
+++ Image distorsion/main.js
@@ -1,1 +1,9 @@
 console.log('Image distorsion')
+
+let container = document.querySelector('.container')
+
+for (let i = 0; i < 400; i++) {
+    let block = document.createElement('div')
+    block.classList.add('.block')
+    container.appendChild(block)
+}
